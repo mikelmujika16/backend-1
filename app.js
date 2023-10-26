836c25ce082ec2b4a00e55163477014928ed9443
mikelmujika16 <124912823+mikelmujika16@users.noreply.github.com>
2023-10-26 13:19:54 +0000

    mhjvj

--- app.js
+++ app.js
@@ -9,9 +9,31 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", function(req, res) {
     res.render('index', {
-    'izenburua': 'EJS probatzen'
+    'izenburua': 'bezeroakAriketa',
+    'bezeroak': bezeroak
     })
-});   
+});  
+
+let bezeroak = [
+    {
+    id: 1,
+    izena: 'Ane',
+    abizena: 'Uriarte',
+    email: 'ane@ni.eus'
+    },
+    {
+    id: 2,
+    izena: 'Jon',
+    abizena: 'Juanenea',
+    email: 'jon@ni.eus'
+    },
+    {
+    id: 3,
+    izena: 'Oihane',
+    abizena: 'Lete',
+    email: 'oihane@ni.eus'
+    },
+    ]
 
  app.listen( 3000, function() {
      console.log("Zerbitzaria 3000 portuan entzuten");
@@ -19,8 +41,21 @@ app.get("/", function(req, res) {
 
 //bezeroa post moduan
 app.post("/bezeroa", function(req,res) {
-    res.send(`Kaixo ${req.body.izena} ${req.body.abizena}`);
+    bezeroak.push({
+        id: bezeroak.length,
+        izena: req.body.izena,
+        abizena: req.body.abizena,
+        email: req.body.korreoa
+    });
+    res.redirect('/')
 });
+
+app.get('/delete/:email', function(req, res) {
+    let bezeroak2 = bezeroak.filter( bezeroa => bezeroa.email!=req.params.email );
+    bezeroak = bezeroak2;
+});
+
+
 
 app.get('/bezeroa/:izena', function(req, res) {
     res.send("Bezeroaren izena:" + req.params.izena);
